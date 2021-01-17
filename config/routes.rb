01e17223fcf54eed951239c05eee1d7915b5d10e
Bruno Julião <brunojuliao.net@gmail.com>
2021-01-16 23:44:59 -0300

    User management working

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
   root 'home#index'
   namespace :admin do
     get '', to: 'home#index'
-
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
-
     resources :sessions, only: %i[create destroy]
-
     post 'login', to: 'sessions#create'
+    resources :users
   end
 end
